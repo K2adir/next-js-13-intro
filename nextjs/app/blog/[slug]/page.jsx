7bcb951a307570eae7e126725e863e681de2c3fe
K2adir <k2adir@gmail.com>
2023-03-27 21:03:26 -0400

    layout and grouping demo added

---
+++ nextjs/app/blog/[slug]/page.jsx
@@ -0,0 +1,5 @@
+export default function Post() {
+  return (
+    <div> BLOG POST PAGE - This is how slugs work. create a folder with []</div>
+  );
+}
---
+++ nextjs/app/contact/page.jsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <>
+      <h1>Contact PAGE - this is the children</h1>
+    </>
+  );
+}
